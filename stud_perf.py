33fda008e572e6dfc032b66e227405e5a7a0fe21
Nuramuiop <92998124+Nuramuiop@users.noreply.github.com>
2025-06-20 15:58:47 +0700

    Update stud_perf.py

--- stud_perf.py
+++ stud_perf.py
@@ -4,7 +4,7 @@ import joblib
 from sklearn.preprocessing import LabelEncoder
 
 model = joblib.load("model_gb.pkl")
-le = LabelEncoder()
+le = joblib.load("encoder.pkl")
 
 col1, col2 = st.columns([1, 5])
 with col1:
